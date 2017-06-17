814f70d5ed80d3ca2f6fa420848c673d3741a32e
Steven Wallace <snwallace@gmail.com>
2017-06-17 13:12:19 -0500

    Added deletion.

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ import io
 from ctypes import *
 from skiplistcollections import SkipListDict
 
+
 class Index:
     def __init__(self, *keys):
         self.keys = keys
@@ -43,17 +44,52 @@ class Database:
                 _fields_ = fields
                 table = cls
 
+                __dirty = False
+
+                @property
+                def _dirty(self):
+                    return self.__dirty
+
+                @_dirty.setter
+                def _dirty(self, value):
+                    if value == self.__dirty:
+                        return
+                    cls._dirty_index.remove(self)
+                    self.__dirty = value
+                    cls._dirty_index.add(self)
+
                 def __init__(self):
                     cls.max_id += 1
                     self.id = cls.max_id
                     self.table.rows.append(self)
+                    self._offset = self.table.max('_offset', -1) + 1
+                    for index in self.table.indices:
+                        index.add(self)
+                    self._dirty = True
+
+                def destroy(self):
+                    for index in cls.indices:
+                        index.remove(self)
+                    cls.rows.remove(self)
+                    offset_index = cls.find_index('_offset')
+                    try:
+                        max = next(iter(offset_index.find(None, True)))
+                        if max._offset > self._offset:
+                            offset_index.remove(max)
+                            max._offset = self._offset
+                            max._dirty = True
+                            offset_index.add(max)
+                    except StopIteration:
+                        pass
 
             for column in cls.columns:
                 setattr(Row, column.name, column.Property())
             cls.Row = Row
             cls.max_id = -1
 
-            cls.indices = [Index('id')]
+            cls.indices = [Index('id'), Index('_offset', 'id'), Index('_dirty', 'id')]
+
+            cls._dirty_index = cls.find_index('_dirty')
 
         @classmethod
         def load(cls, path):
@@ -73,14 +109,46 @@ class Database:
                     column.load(row)
                 for index in cls.indices:
                     index.add(row)
+                row._dirty = False
             cls.max_id = max((i.id for i in cls.rows), default=0)
 
         @classmethod
         def save(cls, path):
             path = path.joinpath(cls.__name__ + '.tbl')
-            file = open(path, 'wb')
-            for row in cls.rows:
+            file = open(path, 'r+b')
+            size = sizeof(cls.Row)
+            dirty = [i for i in cls._dirty_index.find((True,))]
+            for row in dirty:
+                file.seek(row._offset * size)
                 file.write(row)
+                row._dirty = False
+            highest_offset = cls.max('_offset', -1) + 1
+            file.truncate(highest_offset * size)
+
+        @classmethod
+        def find_index(cls, keys):
+            match = -1
+            best_index = None
+            for index in cls.indices:
+                strength = 0
+                for key in index.keys:
+                    if key in keys:
+                        strength += 1
+                    else:
+                        break
+                if strength > match:
+                    match = strength
+                    best_index = index
+            return best_index
+
+        @classmethod
+        def max(cls, key, default=None):
+            try:
+                index = cls.find_index(key)
+                max = next(iter(index.find(None, True)))
+                return getattr(max, key)
+            except StopIteration:
+                return default
 
     def __init__(self):
         self.Table.db = self
@@ -106,6 +174,7 @@ class Column:
             def __set__(prop, instance, value):
                 instance._dirty = True
                 return self.set(instance, value)
+
         self.Property = Property
 
     def __set_name__(self, owner, name):
@@ -186,7 +255,6 @@ class TestTable(db.Table):
 
 
 db.connect('test_database')
-row = TestTable.Row()
-row.num = row.id
-row.str = 'test'
+row = TestTable.rows[0]
+print(row.id)
 db.save()
